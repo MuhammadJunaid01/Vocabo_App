8d4a36ce50e7bcc748d5ff1ac9f1dff1b6f71671
MuhammadJunaid01 <m.junaidbkh2020@gmail.com>
2026-03-25 23:23:44 +0600

    feat: Initialize app with `AppNavigator` and `SafeAreaProvider`, replacing the sample `NewAppScreen`.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
+import React from 'react';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
-  useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import { AppNavigator } from './src/navigation/AppNavigator';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -24,14 +17,12 @@ function App() {
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <SafeAreaProvider>
+      <AppNavigator />
+    </SafeAreaProvider>
     </View>
   );
 }
